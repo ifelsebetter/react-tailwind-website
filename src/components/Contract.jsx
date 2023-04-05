6800c27775fa1cf8ba546013b86e086f133e685f
ifelsebetter <101323049+ifelsebetter@users.noreply.github.com>
2023-04-05 13:13:04 +0700

    .

--- src/components/Contract.jsx
+++ src/components/Contract.jsx
@@ -14,7 +14,7 @@ const Contract = () => {
     <div className='max-w-[760px] mt-[-96px] w-full h-screen mx-auto flex flex-col items-center justify-center'>
       <ul className="list-none p-0 m-0">
         <li>
-        <button onClick={handleDiscordClick} className='border rounded-md p-4 my-4 flex items-center hover:bg-[#A9D9D9] bg-gray-100'>
+          <button onClick={handleDiscordClick} className='w-full border rounded-md p-4 my-4 flex items-center hover:bg-[#A9D9D9] bg-gray-100'>
           <BsDiscord size={35} className="mr-4 text-[#7289DA]" />
           <p className="text-xl font-medium">Discord</p>
         </button>
