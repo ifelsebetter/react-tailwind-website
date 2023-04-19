1cf7cf7a6091f694f9b6cd005d76dd4393fee2a9
ifelsebetter <101323049+ifelsebetter@users.noreply.github.com>
2023-04-19 17:31:54 +0700

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,65 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
-import { BiHomeAlt2, BiUser } from 'react-icons/bi'
-import { CgMail } from 'react-icons/cg'
-import { GiGalaxy } from 'react-icons/gi'
+import { useState, useEffect } from "react";
+import { GiGalaxy } from "react-icons/gi";
+import { BiHomeAlt2, BiUser } from "react-icons/bi";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai"
+import { CgMail } from "react-icons/cg";
+import './Navbar.css';
 
 const Navbar = () => {
-  const [button, setButton] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const MenuButton = () => {
-    setButton(!button);
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth > 768) {
-        setButton(false);
+        setIsMenuOpen(false);
       }
     };
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
-    <nav className='flex justify-between items-center h-24 mx-auto px-4 text-white bg-[#191919]'>
+    <nav className="flex justify-between items-center h-24 mx-auto px-4 text-white bg-[#191919]">
       <button>
         <GiGalaxy size={50} />
       </button>
-      <ul className='hidden md:flex'>
-        <li className='p-4'>
-          <a href="Home">Home</a>
+
+      <ul className="hidden md:flex">
+        <li className="p-4">
+          <a href="/">Home</a>
         </li>
-        <li className='p-4'>
-          <a href="About">About</a>
+        <li className="p-4">
+          <a href="/about">About</a>
         </li>
-        <li className='p-4'>
-          <a href="Contract">Contract</a>
+        <li className="p-4">
+          <a href="/contact">Contact</a>
         </li>
       </ul>
-      <div onClick={MenuButton} className='block md:hidden' aria-label="Menu">
-        {button ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
-      </div>
-      <ul className={button ? 'fixed left-0 top-0 w-[35%] h-full border-r border-r-gray-900 bg-[#191919] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'} aria-hidden={!button}>
-        <li className='m-6 text-left align-bottom'>
-          <a href="Home">
+
+      <button
+        className="block md:hidden"
+        onClick={handleMenuToggle}
+        aria-label="Menu"
+      >
+        {isMenuOpen ? (
+          <AiOutlineClose size={30} />
+        ) : (
+          <AiOutlineMenu size={30} />
+        )}
+      </button>
+
+      <ul
+        className={`${isMenuOpen ? "navbar-menu open" : "navbar-menu"}`}
+        aria-hidden={!isMenuOpen}
+      >
+        <li className="m-6 text-left align-bottom">
+          <a href="/">
             <GiGalaxy size={50} />
           </a>
         </li>
-        <li className='p-4 border-t border-b border-gray-600 flex items-center'>
-
-          <BiHomeAlt2 size={23}/>
-          <a href="Home" className='ml-3'>Home</a>
+        <li className="p-4 border-t border-b border-gray-600 flex items-center">
+          <BiHomeAlt2 size={23} />
+          <a href="/" className="ml-3">
+            Home
+          </a>
         </li>
-        <li className='p-4 border-b border-gray-600 flex items-center'>
-          <BiUser size={23}/>
-          <a href="About" className='ml-3'>About</a>
+        <li className="p-4 border-b border-gray-600 flex items-center">
+          <BiUser size={23} />
+          <a href="/about" className="ml-3">
+            About
+          </a>
         </li>
-        <li className='p-4 border-b border-gray-600 flex items-center'>
-          <CgMail size={23}/>
-          <a href="Contract" className='ml-3'>Contract</a>
+        <li className="p-4 border-b border-gray-600 flex items-center">
+          <CgMail size={23} />
+          <a href="/contact" className="ml-3">
+            Contact
+          </a>
         </li>
       </ul>
     </nav>
