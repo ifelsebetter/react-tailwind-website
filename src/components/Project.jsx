10cdcd247334b28e3d8efea48f3ac411a9cf0961
ifelsebetter <tatsuhiro1808@gmail.com>
2024-03-18 16:49:36 +0700

    remove one repo

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,26 +3,13 @@ import { FaCircle } from "react-icons/fa";
 
 const Project = () => {
   return (
-    <div className="max-w-3xl mx-auto my-20 px-6 text-white">
+    <div className="max-w-4xl mx-auto my-20 px-6 text-white">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         <div>
           <div className="border border-gray-300 rounded-lg p-6 bg-[#191919] hover:bg-[#282828]">
             <h2 className="text-2xl font-bold mb-4">
               <a href="https://github.com/ifelsebetter/Discord-Bot-Js" target="_blank" rel="noopener noreferrer" className="text-white hover:underline" >
-                Discord-Bot-Js
-              </a>
-            </h2>
-            <div className="flex items-center">
-              <FaCircle className="text-3xl md:text-4xl mr-2 text-[#f1e05a]" size={15} />
-              <span className="text-base md:text-lg">JavaScript</span>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="border border-gray-300 rounded-lg p-6 bg-[#191919] hover:bg-[#282828]">
-            <h2 className="text-2xl font-bold mb-4">
-              <a href="https://github.com/ifelsebetter/slash-command-bot" target="_blank" rel="noopener noreferrer" className="text-white hover:underline" >
-                slash-command-bot
+              Discord-Bot-Slash-Command 
               </a>
             </h2>
             <div className="flex items-center">
